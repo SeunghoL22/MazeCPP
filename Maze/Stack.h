e231b83ff654541bc60f9d761c94119bddd9c4eb
LEE SEUNG HO <117427127+SeunghoL22@users.noreply.github.com>
2024-05-08 15:10:08 +0900

    Update Stack.h

--- Maze/Stack.h
+++ Maze/Stack.h
@@ -1,29 +1,28 @@
 #pragma once
-
+#include "Vector.h"
 
 template<typename T>
 class Stack
 {
 public:
-	Stack(){}
-
 	void push(const T& value)
 	{
 		_container.push_back(value);
-	} 
+	}
 
 	void pop()
 	{
 		_container.pop_back();
 	}
 
-	T$ top()
+	T& top()
 	{
-		return _container.back;
+		return _container.back();
 	}
 
-	bool empty() { return size()>0; } //내용물이 존재하는지 여부
+	bool empty() { return size() > 0; }
 	int size() { return _container.size(); }
+
 private:
 	Vector<T> _container;
 };
